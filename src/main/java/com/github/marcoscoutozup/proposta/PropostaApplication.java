1eb6e5c3b2691b0bcba87465ffc63dc8da28b89d
Marcos Couto <marcos.couto@zup.com.br>
2020-10-16 15:35:48 -0300

    Primeiro commit

---
+++ src/main/java/com/github/marcoscoutozup/proposta/PropostaApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcoscoutozup.proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/marcoscoutozup/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcoscoutozup.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
