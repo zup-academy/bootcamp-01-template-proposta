0b1706cd4c3fd0da3383055e7b798d0024106ad6
origin <evertoncnsouza@gmail.com>
2020-10-23 11:51:15 -0300

    Iniciando o projeto

---
+++ src/main/java/io/github/evertocnsouza/domain/entity/Cadastro.java
@@ -0,0 +1,4 @@
+package io.github.evertocnsouza.domain.entity;
+
+public class Cadastro {
+}
---
+++ src/main/java/io/github/evertocnsouza/domain/entity/PropostaApplication.java
@@ -0,0 +1,13 @@
+package io.github.evertocnsouza.domain.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class PropostaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropostaApplication.class, args);
+    }
+}
