bdfeadfa61fc1f2bb77be489437ec57bca8e5c4a
Marcelo Amorim <marcelo.med.amorim@gmail.com>
2020-10-25 19:31:21 -0300

    adiciona projeto inicial e cria serviços via docker-compose

---
+++ proposta/src/main/java/br/com/proposta/PropostaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaApiApplication.class, args);
+	}
+
+}
