b9906703e92066ffb4985e3971710793ad2563a9
Matheus Franco <matheus_2222@hotmail.com>
2020-10-27 09:15:37 -0300

    Criando projeto

---
+++ src/main/java/br/com/cartao/proposta/PropostaCartaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.cartao.proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaCartaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaCartaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cartao/proposta/PropostaCartaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cartao.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaCartaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
