b7a68a615fe0055388f6a4aef3b649a38bb67d55
rafreitas94 <62473394+rafreitas94@users.noreply.github.com>
2020-10-27 10:02:41 -0300

    Versão Inicial

---
+++ src/main/java/br/com/itau/cartaobrancoproposta/CartaoBrancoPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.cartaobrancoproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaoBrancoPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaoBrancoPropostaApplication.class, args);
+	}
+
+}
