032f412cfabacbaa40355c5ddb5e867bbef1d6aa
Fernanda Vedovello <fe.vedovello@gmail.com>
2020-10-27 10:18:31 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/cartaoproposta/CartaoPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.cartaoproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaoPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaoPropostaApplication.class, args);
+	}
+
+}
