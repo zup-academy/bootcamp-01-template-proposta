194bdc2bdc946afd14b829a7a4efec95ca1c9bee
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-11-05 19:47:18 -0300

    response proposta

---
+++ src/main/java/br/com/zup/bootcamp/proposta/api/dto/ResponsePropostaDto.java
@@ -0,0 +1,33 @@
+package br.com.zup.bootcamp.proposta.api.dto;
+
+import br.com.zup.bootcamp.proposta.domain.entity.Proposta;
+import br.com.zup.bootcamp.proposta.domain.service.StatusProposta;
+
+public class ResponsePropostaDto {
+
+    private final String nome;
+    private final String email;
+    private final StatusProposta status;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public StatusProposta getStatus() {
+        return status;
+    }
+
+    public ResponsePropostaDto(String nome, String email, StatusProposta status) {
+        this.nome = nome;
+        this.email = email;
+        this.status = status;
+    }
+
+    public static ResponsePropostaDto toDto(Proposta proposta) {
+        return new ResponsePropostaDto(proposta.getNome(), proposta.getEmail(), proposta.getStatus());
+    }
+}
