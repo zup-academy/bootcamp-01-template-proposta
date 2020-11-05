7aafe283514999af6dcba041a3fcbfddd115ecb1
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-11-05 11:01:38 -0300

    Adicionando logger

--- src/main/java/br/com/zup/bootcamp/proposta/api/controller/CadastraPropostaController.java
+++ src/main/java/br/com/zup/bootcamp/proposta/api/controller/CadastraPropostaController.java
@@ -2,45 +2,44 @@ package br.com.zup.bootcamp.proposta.api.controller;
 
 
 import br.com.zup.bootcamp.proposta.api.dto.RequestPropostaDto;
-import br.com.zup.bootcamp.proposta.api.handler.VerificaDocumentoCpfCnpjValidator;
 import br.com.zup.bootcamp.proposta.domain.repository.PropostaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.zup.bootcamp.proposta.domain.service.PropostaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 @RestController
 public class CadastraPropostaController {
 
-    private VerificaDocumentoCpfCnpjValidator documentoValidator;
+    private final Logger logger = LoggerFactory.getLogger(CadastraPropostaController.class);
+    //1
+    private PropostaService service;
+                    //1
     private PropostaRepository repository;
 
-    public CadastraPropostaController(VerificaDocumentoCpfCnpjValidator documentoValidator, PropostaRepository repository) {
-        this.documentoValidator = documentoValidator;
+    public CadastraPropostaController(PropostaService service, PropostaRepository repository) {
+        this.service = service;
         this.repository = repository;
-    }
-
-    @InitBinder
-    public void init(WebDataBinder binder) {
-        binder.addValidators(documentoValidator);
     }
 
     @PostMapping(value = "/propostas")                          //1
     public ResponseEntity<?> adiciona(@RequestBody @Valid RequestPropostaDto request, UriComponentsBuilder uriComponentsBuilder) {
+        var ultimosDigitosDocumento = request.getDocumento().substring(request.getDocumento().length()-5);
+        logger.info("Nova request recebida. Final do documento {} ", ultimosDigitosDocumento);
         //1
        if (repository.findByDocumento(request.getDocumento()).isPresent()){
+           logger.warn("Documento com o final {} ja consta na base de dados", ultimosDigitosDocumento);
            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
        }
-            //1
-       var proposta = request.toEntity();
-       repository.save(proposta);
-       return ResponseEntity.created(uriComponentsBuilder.path("/proposta/{id}").buildAndExpand(proposta.getId()).toUri()).build();
+       var propostaAvaliada = service.salvaProposta(request.toEntity());
+        logger.info("Proposta {} e com documentocom o final {} adiciona com sucesso", propostaAvaliada.getId(),
+                propostaAvaliada.getDocumento().substring(propostaAvaliada.getDocumento().length()-5));
+
+       return ResponseEntity.created(uriComponentsBuilder.path("/proposta/{id}").buildAndExpand(propostaAvaliada.getId()).toUri()).build();
     }
 }
