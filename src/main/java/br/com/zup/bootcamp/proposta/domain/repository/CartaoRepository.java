dc88f669ab8c12965d1783e12fb9797943691734
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-11-05 19:45:21 -0300

    adicionadno anotaçao @Repository

--- src/main/java/br/com/zup/bootcamp/proposta/domain/repository/CartaoRepository.java
+++ src/main/java/br/com/zup/bootcamp/proposta/domain/repository/CartaoRepository.java
@@ -2,6 +2,9 @@ package br.com.zup.bootcamp.proposta.domain.repository;
 
 import br.com.zup.bootcamp.proposta.domain.entity.Cartao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface CartaoRepository extends JpaRepository<Cartao, String> {
 }
