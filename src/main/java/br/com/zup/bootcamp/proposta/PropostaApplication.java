aa3f0d614cb2555af4eec868f703c96bf0c956e2
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-11-05 11:07:16 -0300

    habilitando feign

--- src/main/java/br/com/zup/bootcamp/proposta/PropostaApplication.java
+++ src/main/java/br/com/zup/bootcamp/proposta/PropostaApplication.java
@@ -2,8 +2,10 @@ package br.com.zup.bootcamp.proposta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = {"br.com.zup.bootcamp.proposta.api.externalsystem", "br.com.zup.bootcamp.proposta.api.controller"})
 public class PropostaApplication {
 
 	public static void main(String[] args) {
