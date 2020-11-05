ac22ddaf9e8d962c680a611aa3481374199fb768
thaismathias <thais-mathias@hotmai.com>
2020-11-05 19:28:29 -0300

    Adicionando configuração de Segurança
    -Adicionadno dependências para OAuth2 no pom.xml
    -Adicionando classe SecurityConfiguration.java para configurar autorizações

---
+++ src/main/java/com/proposta/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.proposta.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.GET, "/actuator/health").permitAll()
+                        .antMatchers(HttpMethod.GET, "/api/propostas/**").hasAuthority("SCOPE_meu-primeiro-escopo")
+                        .antMatchers(HttpMethod.GET, "/api/cartoes/**").hasAuthority("SCOPE_meu-primeiro-escopo")
+                        .antMatchers(HttpMethod.POST, "/api/cartoes/**").hasAuthority("SCOPE_meu-primeiro-escopo")
+                        .antMatchers(HttpMethod.POST, "/api/propostas/**").hasAuthority("SCOPE_meu-primeiro-escopo")
+                        .anyRequest().authenticated()
+        )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
