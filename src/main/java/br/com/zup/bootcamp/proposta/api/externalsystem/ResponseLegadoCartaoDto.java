f1e41a64a5a1ab5ce7022dbe545b82c6e780db85
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-11-05 11:11:15 -0300

    dto response legado do Cartao

---
+++ src/main/java/br/com/zup/bootcamp/proposta/api/externalsystem/ResponseLegadoCartaoDto.java
@@ -0,0 +1,44 @@
+package br.com.zup.bootcamp.proposta.api.externalsystem;
+
+import br.com.zup.bootcamp.proposta.domain.entity.Cartao;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class ResponseLegadoCartaoDto {
+    private UUID id;
+    private LocalDateTime emitidoEm;
+    private String titular;
+    private BigDecimal limite;
+
+    public Cartao toEntity(){
+        return new Cartao(id, emitidoEm, titular, limite);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public LocalDateTime getEmitidoEm() {
+        return emitidoEm;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public BigDecimal getLimite() {
+        return limite;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseLegadoCartaoDto{" +
+                "id=" + id +
+                ", emitidoEm=" + emitidoEm +
+                ", titular='" + titular + '\'' +
+                ", limite=" + limite +
+                '}';
+    }
+}
