bdd2b55ca2ec32decbe374807bfdf976a7a15b58
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-11-09 18:49:09 -0300

    chamada ao sistema de bloqueio legado

--- src/main/java/br/com/zup/bootcamp/proposta/api/externalsystem/LegadoSistemaCartao.java
+++ src/main/java/br/com/zup/bootcamp/proposta/api/externalsystem/LegadoSistemaCartao.java
@@ -1,12 +1,18 @@
 package br.com.zup.bootcamp.proposta.api.externalsystem;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "SistemaCartao", url = "${cartao.host}" )
+import java.util.Map;
+import java.util.UUID;
+
+@FeignClient(name = "SistemaCartao", url = "${cartao.host}")
 public interface LegadoSistemaCartao {
 
     @GetMapping("/cartoes")
     ResponseLegadoCartaoDto consultaCartaoPorIdProposta(@RequestParam String idProposta);
+
+    @PostMapping("/cartoes/{idCartao}/bloqueios")
+    ResponseEntity<?> bloqueiaCartaoPorIdCartao(@PathVariable UUID idCartao, @RequestBody Map<String, String> params);
 }
