1c4ca431626b11e1bc9dcb1627a48a5615ce4cee
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-11-09 18:46:23 -0300

    organizando projeto

--- src/main/java/br/com/zup/bootcamp/proposta/api/controller/CadastraPropostaController.java
+++ src/main/java/br/com/zup/bootcamp/proposta/api/controller/CadastraPropostaController.java
@@ -3,7 +3,7 @@ package br.com.zup.bootcamp.proposta.api.controller;
 
 import br.com.zup.bootcamp.proposta.api.dto.RequestPropostaDto;
 import br.com.zup.bootcamp.proposta.domain.repository.PropostaRepository;
-import br.com.zup.bootcamp.proposta.domain.service.PropostaService;
+import br.com.zup.bootcamp.proposta.domain.service.CadastrarPropostaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -18,26 +18,26 @@ public class CadastraPropostaController {
 
     private final Logger logger = LoggerFactory.getLogger(CadastraPropostaController.class);
                     //1
-    private PropostaService service;
+    private CadastrarPropostaService service;
                     //1
     private PropostaRepository repository;
 
-    public CadastraPropostaController(PropostaService service, PropostaRepository repository) {
+    public CadastraPropostaController(CadastrarPropostaService service, PropostaRepository repository) {
         this.service = service;
         this.repository = repository;
     }
 
     @PostMapping(value = "/propostas")                          //1
-    public ResponseEntity<?> CadastrarProposta(@RequestBody @Valid RequestPropostaDto request, UriComponentsBuilder uri) {
+    public ResponseEntity<?> cadastrarProposta(@RequestBody @Valid RequestPropostaDto request, UriComponentsBuilder uri) {
         var ultimosDigitosDocumento = request.getDocumento().substring(request.getDocumento().length()-5);
         logger.info("Iniciando processo de cadastro de proposta do documento com final {} ", ultimosDigitosDocumento);
         //1
        if (repository.findByDocumento(request.getDocumento()).isPresent()){
            logger.warn("Documento com o final {} ja consta na base de dados", ultimosDigitosDocumento);
-           return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+           return ResponseEntity.notFound().build();
        }
        var propostaAvaliada = service.salvaProposta(request.toEntity());
-        logger.info("Proposta {} e com documentocom o final {} adiciona com sucesso", propostaAvaliada.getId(),
+        logger.info("Proposta id: {} e documento com o final {} adiciona com sucesso", propostaAvaliada.getId(),
                 propostaAvaliada.getDocumento().substring(propostaAvaliada.getDocumento().length()-5));
 
        return ResponseEntity.created(uri.path("/propostas/{id}").buildAndExpand(propostaAvaliada.getId()).toUri()).build();
