9c03cf3ef1c51309d0484c1f2a9490d4d4e93cd5
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-11-09 18:52:02 -0300

    organizando projeto

--- src/main/java/br/com/zup/bootcamp/proposta/api/dto/ResponsePropostaDto.java
+++ src/main/java/br/com/zup/bootcamp/proposta/api/dto/ResponsePropostaDto.java
@@ -1,7 +1,7 @@
 package br.com.zup.bootcamp.proposta.api.dto;
 
 import br.com.zup.bootcamp.proposta.domain.entity.Proposta;
-import br.com.zup.bootcamp.proposta.domain.service.StatusProposta;
+import br.com.zup.bootcamp.proposta.domain.service.enums.StatusProposta;
 
 public class ResponsePropostaDto {
 
