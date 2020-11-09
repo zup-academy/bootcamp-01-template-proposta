0526d9abf89980a45f15e617876d8df587f6a4ee
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-11-09 18:52:39 -0300

    organizando projeto

--- src/main/java/br/com/zup/bootcamp/proposta/domain/service/enums/StatusProposta.java
+++ src/main/java/br/com/zup/bootcamp/proposta/domain/service/enums/StatusProposta.java
@@ -1,4 +1,4 @@
-package br.com.zup.bootcamp.proposta.domain.service;
+package br.com.zup.bootcamp.proposta.domain.service.enums;
 
 public enum StatusProposta {
     NAO_ELEGIVEL, ELEGIVEL, PENDENTE
