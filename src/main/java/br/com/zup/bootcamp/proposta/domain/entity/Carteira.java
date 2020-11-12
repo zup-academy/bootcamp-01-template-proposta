b4bbeba169d585e88d3ee53818a944541e7a440d
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-11-12 17:26:11 -0300

    cadastrar carteira

--- src/main/java/br/com/zup/bootcamp/proposta/domain/entity/Carteira.java
+++ src/main/java/br/com/zup/bootcamp/proposta/domain/entity/Carteira.java
@@ -1,11 +1,14 @@
 package br.com.zup.bootcamp.proposta.domain.entity;
 
+import br.com.zup.bootcamp.proposta.domain.service.enums.EmpresaAssociada;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
@@ -16,10 +19,54 @@ public class Carteira {
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     private String id;
 
-    @Email
+    @Email @NotBlank
     private String email;
 
-    private LocalDateTime associadaEm;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private EmpresaAssociada carteira;
 
-    private String emissor;
+    @CreationTimestamp
+    private LocalDateTime solicitadoEm;
+
+    @ManyToOne
+    @Valid @NotNull
+    private Cartao cartao;
+
+    @Deprecated
+    public Carteira() {
+    }
+
+    public Carteira(@Email @NotBlank String email, @NotNull EmpresaAssociada carteira) {
+        this.email = email;
+        this.carteira = carteira;
+    }
+
+    public Carteira(@Email @NotBlank String email, @NotNull EmpresaAssociada carteira, @Valid @NotNull Cartao cartao) {
+        this.email = email;
+        this.carteira = carteira;
+        this.cartao = cartao;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public EmpresaAssociada getCarteira() {
+        return carteira;
+    }
+
+    @Override
+    public String toString() {
+        return "Carteira{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", empresa=" + carteira +
+                ", solicitadoEm=" + solicitadoEm +
+                '}';
+    }
 }
