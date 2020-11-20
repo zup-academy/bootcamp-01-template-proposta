f219346b891157079b419ba1cc6166f84143e1ab
luizgustv <luiz_gustavo_28@hotmail.com>
2020-11-20 08:51:48 -0300

    finalizando proposta

--- src/main/java/br/com/zup/proposta/dto/integration/IntegracoesCartao.java
+++ src/main/java/br/com/zup/proposta/dto/integration/IntegracoesCartao.java
@@ -1,16 +1,13 @@
 package br.com.zup.proposta.dto.integration;
 
 import br.com.zup.proposta.dto.CartaoRequest;
-import br.com.zup.proposta.model.enums.StatusCartao;
-import feign.Body;
-import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
-import java.util.UUID;
 
 @FeignClient(url ="${host.cartao}", name = "integracaoCartao")
 public interface IntegracoesCartao {
